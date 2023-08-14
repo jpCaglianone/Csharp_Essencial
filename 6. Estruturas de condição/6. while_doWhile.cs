60cae1f8ed05ffa19322f2bed2888a78c5431800
PC <joao.caglianone@gmail.com>
2023-08-13 23:39:18 -0300

    Inclusão de exemplo de while

---
+++ 6. Estruturas de condição/6. while_doWhile.cs
@@ -0,0 +1,30 @@
+﻿ using System;
+
+namespace while_doWhile
+{  
+	public class exemplo
+	{
+		public exemplo()
+		{
+			int numA = 10;
+			while (numA > 0) // o loop irá ocorrer até essa condição ser atendida
+			{
+				Console.WriteLine(numA);
+				--numA;				
+			}
+
+
+			int numB;
+			do //o do executa a primeira repetição de loop e só depois analisa se a condição é atendida
+			{
+				Console.Write("Escreva um número maior que zero: ");
+                numB = int.Parse(Console.ReadLine());
+
+            }while (numB < 0);
+			
+
+			
+		}
+	}
+}
+ 
--- 6. Estruturas de condição/Condição.cs
+++ 6. Estruturas de condição/Condição.cs
@@ -1,4 +1,5 @@
 ﻿//if_else.exemplo ifelse = new if_else.exemplo();
 //switchcase.exemplo sc = new switchcase.exemplo();
-gotolabel.exemplo gl = new gotolabel.exemplo();
+//gotolabel.exemplo gl = new gotolabel.exemplo();
 
+while_doWhile.exemplo dw = new while_doWhile.exemplo();
