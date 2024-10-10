d9eedad554539e66953951a92b10f1e768270fd6
jpCaglianone <joao.caglianone@gmail.com>
2024-10-09 22:07:01 -0300

    Exemplo sobrecarga

--- 7. Classes/7. Classes.cs
+++ 7. Classes/7. Classes.cs
@@ -1,6 +1,8 @@
 ﻿
 #region Modos_de_instanciação
 
+Console.WriteLine("----------------------------------- Modos de instanciação -----------------------------------");
+
 //existem os 3 modos de instanciação abaixo (considerando uma classe com constructor padrão
 
 ClassePessoa pessoa_1 = new ClassePessoa();
@@ -9,8 +11,12 @@ var pessoa_3 = new ClassePessoa();
 
 #endregion
 
+#region Exemplo_ClassePessoaConstructor
+
 //abaixo, após o objeto pessoa_1 ter sido instanciado a partir da classe ClassePessoa, foram movidos valores para os atributos do objeto
 //obs: esse modo não é uma boa prática
+
+Console.WriteLine("----------------------------------- Constructor -----------------------------------");
 
 pessoa_1.altura = 1.89f;
 pessoa_1.peso = 100;
@@ -24,7 +30,23 @@ pessoa_1.novoPeso(90.5f);
 ClassePessoaConstructor pessoa_4 = new("João"); //uso o constructor que solicita o nome como parametro, a IDE reconhece automaticamente
 Console.WriteLine(pessoa_4.nome);
 
+#endregion
 
+#region Exemplo_ClasseSobrecarga
+
+Console.WriteLine("----------------------------------- Modos de instanciação -----------------------------------");
+
+double a = 1.1;
+double b = 2.2;
+double c = 3.3;
+int x = 4;
+
+ClasseSobrecarga classeSobrecarga = new();
+Console.WriteLine(classeSobrecarga.Somar(a, b)); //invoca a primeira Somar
+Console.WriteLine(classeSobrecarga.Somar(a, b, c)); //invoca a segundo Somar
+Console.WriteLine(classeSobrecarga.Somar(x, a)); //invoca o quarto Somar
+
+#endregion
 
 
 
---
+++ 7. Classes/ClasseSobrecarga.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class ClasseSobrecarga
+{
+	public ClasseSobrecarga(){} //constructor padrão
+
+	//private double a;
+	//private double b;
+
+
+	//teoricamente, não pode haver mais de um método com o mesmo nome na classe ou na herança, entretanto
+	//é possível fazer uma sobrecarga, tendo o mesmo nome de métodos, alterando a quantidade de parâmetros, tipo de parâmetros
+	// e/ou ordem de parâmetros
+
+
+	//primeiro método somar
+	public double Somar(double a, double b)
+	{
+		return a + b;
+	}
+
+	//sobrecargar por quantidade de parametros
+    public double Somar(double a, double b, double c)
+    {
+        return a + b + c;
+    }
+
+	//sobrecarga por tipo de parametros
+    public double Somar(double a, int b)
+    {
+        return a + b;
+    }
+
+	//sobrecarga por ordem de parametros (em relaçao ao anterior)
+	public double Somar (int a, double b)
+	{
+		return a + b;
+	}
+}
